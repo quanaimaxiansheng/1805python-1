af0841637e378ddde68d19bdcb0f0e90c0d1dec3
赵庆元 <qingyuan@geekniu.com>
2018-06-21 08:59:07 +0800

    大佛

---
+++ 15day/01-函数.py
@@ -0,0 +1,30 @@
+print("hello_cai")
+
+print("hello_cai")
+
+print("hello_cai")
+
+
+
+
+
+#封装
+
+'''
+def 函数名():
+    pass
+'''
+
+#声明函数
+def print_hello():
+    print("hello_caizijin")
+
+#调用函数
+print_hello()
+
+print_hello()
+
+print_hello()
+
+
+
